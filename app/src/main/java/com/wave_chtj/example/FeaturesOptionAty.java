61cab6a1bb00d2a541db7d741657712afb48457e
ctj <chentingjian@face-future.com>
2020-03-30 14:11:14 +0800

    /

--- app/src/main/java/com/wave_chtj/example/FeaturesOptionAty.java
+++ app/src/main/java/com/wave_chtj/example/FeaturesOptionAty.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.chtj.base_iotutils.GlobalLoadDialog;
 import com.chtj.base_iotutils.GraphicalToastUtils;
 import com.chtj.base_iotutils.KLog;
 import com.chtj.base_iotutils.ToastUtils;
