f8863f33885c0da265fb8898dafa7bfe91d8caf0
ctj <chentingjian@face-future.com>
2021-04-30 09:59:04 +0800

    修改adb工具类

--- base_iotutils/src/main/java/com/face_chtj/base_iotutils/ShellUtils.java
+++ base_iotutils/src/main/java/com/face_chtj/base_iotutils/ShellUtils.java
@@ -1,32 +1,58 @@
 package com.face_chtj.base_iotutils;
+
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- *
  * @author chtj
  * create by chtj on 2019-8-6
  * desc:adb Shell相关工具类
  * Command执行结果 CommandResult
- *
+ * <p>
  * 使用方式如下:
  * ShellUtils.CommandResult commandResult=ShellUtils.execCommand("reboot",true);//adb执行重启
  * 理论上commandResult.result=0时执行是成功的
  * Log.d(TAG, "commandResult errMeg="+commandResult.errorMsg+",result="+commandResult.result+",successMsg="+commandResult.successMsg);
- *
+ * <p>
  * 这里是常用的方法
  * --执行命令一条 {@link #execCommand(String command, boolean isRoot)}
  * --执行命令-多条 {@link #execCommand(String[] commands, boolean isRoot)}
+ * --检查Root权限 {@link #isCheckRoot()}
  */
 public class ShellUtils {
-    private static final String TAG="ShellUtils";
-    
+    private static final String TAG = "ShellUtils";
+
     public final static String COMMAND_SU = "su";
     public final static String COMMAND_SH = "sh";
     public final static String COMMAND_EXIT = "exit\n";
     public final static String COMMAND_LINE_END = "\n";
+
+    /**
+     * 检查是否拥有root权限
+     * 执行shell adb 命令很多需要root权限
+     * @return
+     */
+    public static boolean isCheckRoot() {
+        File f = null;
+        final String kSuSearchPaths[] = {"/system/bin/", "/system/xbin/", "/system/sbin/", "/sbin/", "/vendor/bin/"};
+        try {
+            for (int i = 0; i < kSuSearchPaths.length; i++) {
+                f = new File(kSuSearchPaths[i] + "su");
+                if (f != null && f.exists()) {
+                    Log.d(TAG, "find su in : " + kSuSearchPaths[i]);
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     /**
      * Command执行结果
@@ -55,8 +81,8 @@ public class ShellUtils {
         DataOutputStream os = null;
         BufferedReader successResult = null;
         BufferedReader errorResult = null;
-        StringBuilder successMsg = null;
-        StringBuilder errorMsg = null;
+        StringBuilder successMsg  = new StringBuilder();;
+        StringBuilder errorMsg = new StringBuilder();
         try {
             process = Runtime.getRuntime().exec(isRoot ? COMMAND_SU : COMMAND_SH);
             os = new DataOutputStream(process.getOutputStream());
@@ -70,9 +96,6 @@ public class ShellUtils {
             os.writeBytes(COMMAND_EXIT);
             os.flush();
             commandResult.result = process.waitFor();
-            //获取错误信息
-            successMsg = new StringBuilder();
-            errorMsg = new StringBuilder();
             successResult = new BufferedReader(new InputStreamReader(process.getInputStream()));
             errorResult = new BufferedReader(new InputStreamReader(process.getErrorStream()));
             String s;
@@ -80,22 +103,12 @@ public class ShellUtils {
             while ((s = errorResult.readLine()) != null) errorMsg.append(s);
             commandResult.successMsg = successMsg.toString();
             commandResult.errorMsg = errorMsg.toString();
-            KLog.i(TAG, commandResult.result + " | " + commandResult.successMsg
-                    + " | " + commandResult.errorMsg);
         } catch (IOException e) {
-            String errmsg = e.getMessage();
-            if (errmsg != null) {
-                KLog.e(TAG, errmsg);
-            } else {
-                e.printStackTrace();
-            }
+            errorMsg.append(e.getMessage());
+            commandResult.errorMsg = errorMsg.toString();
         } catch (Exception e) {
-            String errmsg = e.getMessage();
-            if (errmsg != null) {
-                KLog.e(TAG, errmsg);
-            } else {
-                e.printStackTrace();
-            }
+            errorMsg.append(e.getMessage());
+            commandResult.errorMsg = errorMsg.toString();
         } finally {
             try {
                 if (os != null) os.close();
