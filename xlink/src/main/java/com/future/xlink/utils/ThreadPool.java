72dcdf26c50b91d4b08c2fbef7663e27365a68b6
ctj <chentingjian@face-future.com>
2021-07-31 16:29:57 +0800

    备注线程池工具类

--- xlink/src/main/java/com/future/xlink/utils/ThreadPool.java
+++ xlink/src/main/java/com/future/xlink/utils/ThreadPool.java
@@ -1,5 +1,7 @@
 package com.future.xlink.utils;
 
+import android.util.Log;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
@@ -7,8 +9,15 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * corePoolSize     指定了线程池中的线程数量，它的数量决定了添加的任务是开辟新的线程去执行，还是放到workQueue任务队列中去
+ * maximumPoolSize  指定了线程池中的最大线程数量，这个参数会根据你使用的workQueue任务队列的类型，决定线程池会开辟的最大线程数量；
+ *      当线程数=maxPoolSize，且任务队列已满时，线程池会拒绝处理任务而抛出异常
+ * keepAliveTime    当线程池中空闲线程数量超过corePoolSize时，多余的线程会在多长时间内被销毁
+ *
+ */
 public class ThreadPool {
-
+    private static final String TAG = "ThreadPool";
     public static class Builder {
         private static ExecutorService executors = new ThreadPoolExecutor(20, 20, 0, TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue <>(), new ThreadFactoryImpl());
