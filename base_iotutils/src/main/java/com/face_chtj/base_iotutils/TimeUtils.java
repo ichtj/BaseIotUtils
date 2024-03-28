7d86b243cee054074ba77bd74d63e00b7d476cf0
ichtj <13652412606@163.com>
2024-03-28 15:33:58 +0800

    优化时间工具类

--- base_iotutils/src/main/java/com/face_chtj/base_iotutils/TimeUtils.java
+++ base_iotutils/src/main/java/com/face_chtj/base_iotutils/TimeUtils.java
@@ -10,8 +10,8 @@ import java.util.Locale;
  * Create on 2020/3/9
  * author chtj
  * desc 时间工具类
- *
- * {@link #timestampToDate(String,long)} 获取今天年月日
+ * <p>
+ * {@link #timestampToDate(String, long)} 获取今天年月日
  * {@link #getTodayDate()} 获取今天年月日
  * {@link #getTodayDateHms(String)} 获取今天年月日 可添加格式
  * {@link #getTime()} 获取当前系统的时间戳
@@ -19,10 +19,9 @@ import java.util.Locale;
  * {@link #getUTCTimeStr()} 得到UTC时间，类型为字符串，格式为"yyyy-MM-dd HH:mm
  * {@link #differentDaysByMillisecond(Date, Date)} 两个Date间隔天数计算
  * {@link #getWeekOfDate()} 获取当前日期是星期几
- *
  */
 public class TimeUtils {
-//    public static final String TIME_YMD="yyyy-MM-dd";
+    //    public static final String TIME_YMD="yyyy-MM-dd";
 //    public static final String TIME_YMDHM="yyyy-MM-dd HH:mm";
 //    public static final String TIME_YMDHMS="yyyy-MM-dd HH:mm:ss";
 //    public static final String TIME_YMDHMS_CN="yyyy年MM月dd日 HH时mm分ss秒";
@@ -39,23 +38,27 @@ public class TimeUtils {
     public static final String DATE_FORMAT_WEEKDAY = "EEEE"; // 星期几，例如：星期一、星期二
     public static final String DATE_FORMAT_MONTH = "MMMM"; // 月份，例如：一月、二月
     public static final String DATE_FORMAT_YEAR = "yyyy"; // 年份，例如：2024
+
     /**
      * 时间戳 转 String类型的年月日
-     * @param time
-     * @return
+     *
+     * @param time 时间戳
+     * @return 指定格式的时间
      */
-    public static String timestampToDate(String pattern,long time) {
-        return new SimpleDateFormat(pattern, Locale.CHINA).format(time * 1000);
+    public static String timestampToDate(String pattern, long time) {
+        return new SimpleDateFormat(pattern, Locale.getDefault()).format(new Date(time));
     }
+
     /**
      * 获取今天年月日
      */
     public static String getTodayDate() {
-        return new SimpleDateFormat(DATE_FORMAT_DATE_ONLY, Locale.CHINA).format(new Date());
+        return new SimpleDateFormat(DATE_FORMAT_DATE_ONLY, Locale.getDefault()).format(new Date());
     }
 
     /**
      * 获取今天年月日时分秒
+     *
      * @return 2017-08-14 11:53:52
      */
     public static String getTodayDateHms(String pattern) {
@@ -64,6 +67,7 @@ public class TimeUtils {
 
     /**
      * 获取当前系统秒级别的时间戳
+     *
      * @return 1502697135
      */
     public static int getTime() {
@@ -72,6 +76,7 @@ public class TimeUtils {
 
     /**
      * 获取当前系统毫秒的时间戳
+     *
      * @return 1502697135
      */
     public static long getTimeMs() {
@@ -80,13 +85,14 @@ public class TimeUtils {
 
     /**
      * 获取今天添加月份的日期
+     *
      * @param month 添加的月份
      * @return 日期
      */
     public static String getTodayAddMonthDate(int month) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_DATE_ONLY);
-            Date now =sdf.parse(getTodayDate());
+            Date now = sdf.parse(getTodayDate());
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(now);
             calendar.add(Calendar.MONTH, month);
@@ -99,12 +105,13 @@ public class TimeUtils {
     /**
      * 得到UTC时间，类型为字符串，格式为"yyyy-MM-dd HH:mm
      * 如果获取失败，返回null
+     *
      * @return
      */
     public static String getUTCTimeStr() {
         StringBuffer UTCTimeBuffer = new StringBuffer();
         // 1、取得本地时间：
-        Calendar cal = Calendar.getInstance() ;
+        Calendar cal = Calendar.getInstance();
         // 2、取得时间偏移量：
         int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);
         // 3、取得夏令时差：
@@ -112,20 +119,21 @@ public class TimeUtils {
         // 4、从本地时间里扣除这些差量，即可以取得UTC时间：
         cal.add(java.util.Calendar.MILLISECOND, -(zoneOffset + dstOffset));
         int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH)+1;
+        int month = cal.get(Calendar.MONTH) + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
         int hour = cal.get(Calendar.HOUR_OF_DAY);
         int minute = cal.get(Calendar.MINUTE);
         int second = cal.get(Calendar.SECOND);
         UTCTimeBuffer.append(year).append("-").append(month).append("-").append(day);
-        UTCTimeBuffer.append("T").append(hour).append(":").append(minute).append(":").append(second) ;
+        UTCTimeBuffer.append("T").append(hour).append(":").append(minute).append(":").append(second);
         return UTCTimeBuffer.toString();
     }
 
 
     /**
      * 两个Date间隔天数计算
-     * @param after 临近的时间
+     *
+     * @param after  临近的时间
      * @param before 之前的时间
      * @return 间隔天数
      */
